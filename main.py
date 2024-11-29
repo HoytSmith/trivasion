b34fe901f3a17f1b44ac0cb8c93dba98b5e35902
HoytSmith <h.smith@live.nl>
2024-11-29 15:38:23 +0100

    Basic PyGame Loop structure

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ def main():
     screen = pygame.display.set_mode()
 
     #game loop
-    while True:
+    game_is_running = True
+    while game_is_running:
         #events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                game_is_running = False
+        #tick update logic
+        #draw update logic
+        pygame.display.flip()
 
 #ensure that main program is called
 if __name__ == "__main__":
