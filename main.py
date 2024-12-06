c5fdae5d312e01eb4c4f64d7e68b35dedfabd9e8
HoytSmith <h.smith@live.nl>
2024-12-06 13:50:20 +0100

    Added clock for limiting fps

--- main.py
+++ main.py
@@ -11,10 +11,12 @@ current_state = GameState.START
 def main():
     global game_settings, current_state
     screen_resolution = game_settings.get_setting("screen_resolution")
+    fps_limit = game_settings.get_setting("fps_limit")
     #setup pygame
     pygame.init()
     screen = pygame.display.set_mode(screen_resolution)
-
+    clock = pygame.time.Clock()
+    
     #game loop
     game_is_running = True
     while game_is_running:
@@ -24,7 +26,13 @@ def main():
                 game_is_running = False
         #tick update logic
         #draw update logic
+        screen.fill("black")
+        #RENDER LOGIC
         pygame.display.flip()
+
+        #limit tickrate
+        clock.tick(fps_limit)
+
 
 #ensure that main program is called
 if __name__ == "__main__":
