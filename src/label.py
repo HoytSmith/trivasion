25a529f6114f147d11dfec14c42e2eada988a0f2
HoytSmith <h.smith@live.nl>
2024-12-11 13:57:17 +0100

    Updated Label to be more readable and clear.

--- src/label.py
+++ src/label.py
@@ -8,43 +8,64 @@ class Label(GameInterfaceComponent):
         self.set_font_size(font_size)
         super().__init__(name=name, priority=priority, position=position, color=color)
     
-    def set_content(self, content):
+    #VALIDATION METHODS:
+    def validate_content(self, content):
         if not isinstance(content, str):
             raise TypeError("Label text content must be of type string!")
+    
+    def validate_alpha(self, alpha):
+        if not isinstance(alpha, int):
+            raise TypeError("Alpha must be an integer!")
+        if alpha < 0 or alpha > 255:
+            raise ValueError("Alpha value must be at least 0 and at most 255!")
+    
+    def validate_font_size(self, font_size):
+        if not isinstance(font_size, int):
+            raise TypeError("Label font size must be of type integer!")
+        if font_size < 8 or font_size > 100:
+            raise ValueError("Label font size must be greater than 7 and at most 100!")
+    
+    #SETTERS, GETTERS AND OTHER CLASS METHODS:
+    #CONTENT METHODS:
+    def set_content(self, content):
+        self.validate_content(content)
         self.__content = content
     
     def get_content(self):
         return self.__content
-
+    
+    #ALPHA METHODS:
     def set_alpha(self, alpha):
-        if not (isinstance(alpha, int) and alpha >= 0 and alpha <= 255):
-            raise TypeError("Alpha must be an integer of at least 0 and at most 255!")
+        self.validate_alpha(alpha)
         self.__alpha = alpha
 
     def get_alpha(self):
         return self.__alpha
-
+    
+    #FONT SIZE METHODS:
     def set_font_size(self, font_size=24):
-        if not isinstance(font_size, int):
-            raise TypeError("Label font size must be of type integer!")
+        self.validate_font_size(font_size)
         self.__font_size = font_size
     
     def get_font_size(self):
         return self.__font_size
     
-    def set_font(self):
-        self.__font = pygame.font.Font(None, self.get_font_size())
+    #FONT METHODS:
+    def set_font(self, font_path=None):
+        self.__font = pygame.font.Font(font_path, self.get_font_size())
     
     def get_font(self):
         return self.__font
     
+    #TEXT SURFACE METHODS:
     def set_text_surface(self):
         self.__text_surface = self.get_font().render(self.get_content(), True, self.get_color())
         self.set_size(self.__text_surface.get_rect().size)
 
     def get_text_surface(self):
         return self.__text_surface
-
+    
+    #RENDER SURFACE METHODS:
     def set_render(self):
         self.__render = pygame.Surface(self.get_size(), pygame.SRCALPHA)
         self.__render.blit(self.get_text_surface(), (0,0))
@@ -53,14 +74,15 @@ class Label(GameInterfaceComponent):
     def get_render(self):
         return self.__render
     
+    #GAMELOOP METHODS:
     def render(self, screen):
-        #Render cached text surface
         screen.blit(self.get_render(), self.get_position())
     
     def handle_event(self, event, mouse_button_held):
         # Labels are always non-interactive
         return False
     
+    #MAIN UPDATE METHOD
     def update_component(self):
         self.set_font()
         self.set_text_surface()
@@ -71,22 +93,16 @@ class Label(GameInterfaceComponent):
     # EACH OF THESE METHODS INCLUDES A FLAG 'UPDATE_COMPONENT' THAT CAN BE SET TO FALSE
     # TO REDUCE REDUNDANT UPDATE_COMPONENT CALLS FOR CHILD ELEMENTS
     def update_content(self, content, update_component = True):
-        if not isinstance(content, str):
-            raise TypeError("Label text content must be of type string!")
         self.set_content(content)
         if update_component:
             self.update_component()
     
     def update_alpha(self, alpha, update_component = True):
-        if not (isinstance(alpha, int) and alpha >= 0 and alpha <= 255):
-            raise TypeError("Alpha must be an integer of at least 0 and at most 255!")
         self.set_alpha(alpha)
         if update_component:
             self.update_component()
     
     def update_font_size(self, font_size, update_component = True):
-        if not isinstance(font_size, int):
-            raise TypeError("Label font size must be of type integer!")
         self.set_font_size(font_size)
         if update_component:
             self.update_component()
