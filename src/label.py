fe73a7f34f59ea7f9974464cce6ed687d505b5b3
HoytSmith <h.smith@live.nl>
2024-12-11 15:10:51 +0100

    Updated Label class to reflect the implementation of transparency in the base component class

--- src/label.py
+++ src/label.py
@@ -4,9 +4,8 @@ from src.gameinterfacecomponent import GameInterfaceComponent
 class Label(GameInterfaceComponent):
     def __init__(self, name="Label", priority=0, content="Text here...", position=(0,0), color=(255, 255, 255), alpha=255, font_size=24):
         self.set_content(content)
-        self.set_alpha(alpha)
         self.set_font_size(font_size)
-        super().__init__(name=name, priority=priority, position=position, color=color)
+        super().__init__(name=name, priority=priority, position=position, color=color, alpha=alpha)
     
     #VALIDATION METHODS:
     def validate_content(self, content):
@@ -14,12 +13,6 @@ class Label(GameInterfaceComponent):
             raise TypeError("Label text content must be of type string!")
         if content == "":
             raise ValueError("Label text content can not be empty!")
-    
-    def validate_alpha(self, alpha):
-        if not isinstance(alpha, int):
-            raise TypeError("Alpha must be an integer!")
-        if alpha < 0 or alpha > 255:
-            raise ValueError("Alpha value must be at least 0 and at most 255!")
     
     def validate_font_size(self, font_size):
         if not isinstance(font_size, int):
@@ -35,14 +28,6 @@ class Label(GameInterfaceComponent):
     
     def get_content(self):
         return self.__content
-    
-    #ALPHA METHODS:
-    def set_alpha(self, alpha):
-        self.validate_alpha(alpha)
-        self.__alpha = alpha
-
-    def get_alpha(self):
-        return self.__alpha
     
     #FONT SIZE METHODS:
     def set_font_size(self, font_size=24):
@@ -67,18 +52,18 @@ class Label(GameInterfaceComponent):
     def get_text_surface(self):
         return self.__text_surface
     
-    #RENDER SURFACE METHODS:
-    def set_render(self):
+    #LABEL SURFACE METHODS:
+    def set_label_surface(self):
         self.__render = pygame.Surface(self.get_size(), pygame.SRCALPHA)
         self.__render.blit(self.get_text_surface(), (0,0))
         self.__render.set_alpha(self.get_alpha())
     
-    def get_render(self):
+    def get_label_surface(self):
         return self.__render
     
     #GAMELOOP METHODS:
     def render(self, screen):
-        screen.blit(self.get_render(), self.get_position())
+        screen.blit(self.get_label_surface(), self.get_position())
     
     def handle_event(self, event, mouse_button_held):
         # Labels are always non-interactive
@@ -88,7 +73,7 @@ class Label(GameInterfaceComponent):
     def update_component(self):
         self.set_font()
         self.set_text_surface()
-        self.set_render()
+        self.set_label_surface()
         super().update_component()
     
     # THE FOLLOWING ARE THE UPDATE METHODS - EACH CALLS UPDATE_COMPONENT AT THE END
@@ -96,11 +81,6 @@ class Label(GameInterfaceComponent):
     # TO REDUCE REDUNDANT UPDATE_COMPONENT CALLS FOR CHILD ELEMENTS
     def update_content(self, content, update_component = True):
         self.set_content(content)
-        if update_component:
-            self.update_component()
-    
-    def update_alpha(self, alpha, update_component = True):
-        self.set_alpha(alpha)
         if update_component:
             self.update_component()
     
